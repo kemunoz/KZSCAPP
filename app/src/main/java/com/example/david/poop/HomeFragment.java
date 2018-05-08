6dce52f21dd505f1b9f77a774bae8dcf3872dde3
David Ramirez <dabramir@ucsc.edu>
2018-05-08 00:52:11 -0700

    moved play button to MainActivity/ changed theme

--- app/src/main/java/com/example/david/poop/HomeFragment.java
+++ app/src/main/java/com/example/david/poop/HomeFragment.java
@@ -19,52 +19,10 @@ import android.widget.Button;
  */
 
 public class HomeFragment extends Fragment {
-    ImageButton playButton;
-    MediaPlayer mediaPlayer = new MediaPlayer();
+
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
         View v = inflater.inflate(R.layout.fragment_home,container, false);
 
-        playButton = (ImageButton)v.findViewById(R.id.playButton);
-
-        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-
-        final String AudioURL = "http://188.165.192.5:8242/kzschigh?type=.mp3";
-
-        //Preparing audio
-        try {
-            mediaPlayer.setDataSource(AudioURL);
-            mediaPlayer.prepareAsync();
-        } catch (IllegalArgumentException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (SecurityException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (IllegalStateException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch(RuntimeException e) {
-            e.printStackTrace();
-        }
-
-        playButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                //mediaPlayer.start();
-                if (mediaPlayer.isPlaying()) {
-                    mediaPlayer.pause();
-                    //playButton.setText("Play");
-                    playButton.setImageResource(R.drawable.play);
-                }else{
-                    mediaPlayer.start();
-                    playButton.setImageResource(R.drawable.pause);
-                    //playButton.setText("Pause");
-                }
-            }
-        });
 
         return v;
     }
--- app/src/main/java/com/example/david/poop/MainActivity.java
+++ app/src/main/java/com/example/david/poop/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.david.poop;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
@@ -11,18 +15,29 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
+    MediaPlayer mediaPlayer = new MediaPlayer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final FloatingActionButton playButton = (FloatingActionButton) findViewById(R.id.floatingPlayButton);
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
 
         setTitle("KZSC App");
 
@@ -38,6 +53,46 @@ public class MainActivity extends AppCompatActivity {
 
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
+
+        final String AudioURL = "http://188.165.192.5:8242/kzschigh?type=.mp3";
+
+
+        //Preparing audio
+        try {
+            mediaPlayer.setDataSource(AudioURL);
+            mediaPlayer.prepareAsync();
+        } catch (IllegalArgumentException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (SecurityException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IllegalStateException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch(RuntimeException e) {
+            e.printStackTrace();
+        }
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //mediaPlayer.start();
+                if (mediaPlayer.isPlaying()) {
+                    mediaPlayer.pause();
+                    //playButton.setText("Play");
+                    playButton.setImageResource(R.drawable.play);
+                }else{
+                    mediaPlayer.start();
+                    playButton.setImageResource(R.drawable.pause);
+                    //playButton.setText("Pause");
+                }
+            }
+        });
+
 
     }
 
