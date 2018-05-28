8ee8c64a465020f110e8017fe442cd70a3bdde5b
David <daastewa@ucsc.edu>
2018-05-27 17:28:04 -0700

    DJ is distinguished from other users in chat

--- app/src/main/java/com/example/david/poop/ChatFragment.java
+++ app/src/main/java/com/example/david/poop/ChatFragment.java
@@ -39,7 +39,7 @@ import java.util.Map;
 /**
  * Created by David on 4/20/2018.
  */
-public class ChatFragment extends Fragment implements View.OnClickListener {
+public class ChatFragment extends Fragment {
     private ImageButton btn_send_msg;
     private EditText input_msg;
     private ListView chat_conversation;
@@ -49,28 +49,9 @@ public class ChatFragment extends Fragment implements View.OnClickListener {
     private String temp_key;
     private float n = 0;
     private int l =0;
-    private String DJSecretKey = "IAmTheDJ";
-    private boolean isDJ = false;
+    private String DJSecretKey = "DJ";
+    private String isDJ = "False";
 
-    /*public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        root = FirebaseDatabase.getInstance().getReference().child("kzsc-3c2de");
-        btn_send_msg.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //Map<String, Object> map = new HashMap<String, Object>();
-                //temp_key = root.push().getKey();
-                //root.updateChildren(map);
-
-                DatabaseReference message_root =root.child(temp_key);
-                Map<String, Object> map = new HashMap<String, Object>();
-                map.put("name", user_name);
-                map.put("msg", input_msg.getText().toString());
-
-                message_root.updateChildren(map);
-            }
-        });
-    */
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
         savedInstanceState = new Bundle();
         savedInstanceState.putFloat("rn", n);
@@ -80,18 +61,7 @@ public class ChatFragment extends Fragment implements View.OnClickListener {
         chat_conversation = (ListView) rootView.findViewById(R.id.chat_conversation);
        root = FirebaseDatabase.getInstance().getReference().child("kzsc-3c2de");
        request_user_name();
-//       if(n==0) {
-//           n++;
-//           request_user_name();
-//           savedInstanceState.putFloat("rn", n);
-//       }
-      //btn_send_msg.setOnClickListener(new View.OnClickListener() {
-//          @Override
-//          public void onClick(View view) {
-//              Toast.makeText(getActivity(),"Message", Toast.LENGTH_SHORT).show();
-//          }
-//      });
-//        btn_send_msg.setOnClickListener(this);
+
         btn_send_msg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -109,34 +79,7 @@ public class ChatFragment extends Fragment implements View.OnClickListener {
 
             }
         });
-//        root.addChildEventListener(new ChildEventListener() {
-//            @Override
-//            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-//
-//                append_chat_conversation(dataSnapshot);
-//            }
-//
-//            @Override
-//            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-//
-//                append_chat_conversation(dataSnapshot);
-//            }
-//
-//            @Override
-//            public void onChildRemoved(DataSnapshot dataSnapshot) {
-//
-//            }
-//
-//            @Override
-//            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-//
-//            }
-//
-//            @Override
-//            public void onCancelled(DatabaseError databaseError) {
-//
-//            }
-//        });
+
         root.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
@@ -145,7 +88,12 @@ public class ChatFragment extends Fragment implements View.OnClickListener {
                 for (DataSnapshot item_snapshot : dataSnapshot.getChildren()) {
                     Log.d("ITEM", item_snapshot.child("msg").getValue().toString());
                     String nameForJSON = item_snapshot.child("name").getValue().toString();
-                    Toast.makeText(getActivity(),nameForJSON ,Toast.LENGTH_SHORT).show();
+                    if (nameForJSON.equals(DJSecretKey)) {
+                        isDJ = "True";
+                    }
+                    else {
+                        isDJ = "False";
+                    }
                     String messageForJSON = item_snapshot.child("msg").getValue().toString();
                     try {
                         J.put("name", nameForJSON);
@@ -170,36 +118,6 @@ public class ChatFragment extends Fragment implements View.OnClickListener {
 
         return rootView;
     }
-    private String chat_msg, chat_user_name;
-    private void append_chat_conversation(DataSnapshot dataSnapshot) {
-//        Iterator i = dataSnapshot.getChildren().iterator();
-//        List<String> lol = new ArrayList<String>();
-//        while(i.hasNext()){
-//
-//            chat_msg = (String) ((DataSnapshot)i.next()).getValue();
-//            chat_user_name = (String) ((DataSnapshot)i.next()).getValue();
-//            lol.add(chat_msg);
-////        }
-//        }
-
- //       ListAdapter listAd = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_expandable_list_item_1, lol);
- //       chat_conversation.setAdapter(listAd);
-    }
-
-    public void onClick(View View) {
-        //Toast.makeText(getActivity(),"Message", Toast.LENGTH_SHORT).show();
-        Log.d("key value", "blah");
-        //Map<String, Object> map = new HashMap<String, Object>();
-        //temp_key = root.push().getKey();
-        //root.updateChildren(map);
-
-        //DatabaseReference message_root =root.child(temp_key);
-//        Map<String, Object> map = new HashMap<String, Object>();
-//        map.put("name", "kevin");
-//        map.put("msg", input_msg.getText().toString());
-
-       // message_root.updateChildren(map);
-    }
 
     private void request_user_name(){
         AlertDialog.Builder builder = new AlertDialog.Builder(this.getActivity());
@@ -211,16 +129,10 @@ public class ChatFragment extends Fragment implements View.OnClickListener {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
                 name = input_field.getText().toString();
-
                 //Toast.makeText(getActivity(),"outside " + name,Toast.LENGTH_LONG).show();
                 if (name.equals("")) {
                     //Toast.makeText(getActivity(),"blah",Toast.LENGTH_LONG).show();
                     request_user_name();
-                }
-
-                if (name.equals(DJSecretKey)) {
-                    Toast.makeText(getActivity(),"Look at me! I am the DJ Now!",Toast.LENGTH_LONG).show();
-                    isDJ = true;
                 }
             }
         });
--- app/src/main/java/com/example/david/poop/CustomAdapter.java
+++ app/src/main/java/com/example/david/poop/CustomAdapter.java
@@ -1,6 +1,7 @@
 package com.example.david.poop;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -29,9 +30,10 @@ class CustomAdapter extends ArrayAdapter<String>{
         }
 
         String isDJ = json.get("isDJ").toString();
-
+        Log.d("DJKEY", isDJ);
         View customView;
-        if (isDJ == "false") {
+        if (isDJ.equals("False")) {
+            Log.d("INSIDE", "FALSE");
             customView = davidsinflater.inflate(R.layout.custom_row, parent,false);
             TextView textMessage = (TextView) customView.findViewById(R.id.textviewCustom);
             TextView nameMessage = (TextView) customView.findViewById(R.id.textName);
@@ -40,11 +42,13 @@ class CustomAdapter extends ArrayAdapter<String>{
         }
         //The particular entry is from the DJ
         else {
-             customView = davidsinflater.inflate(R.layout.custom_row_dj, parent,false);
-            TextView textMessage = (TextView) customView.findViewById(R.id.textviewCustom);
-            TextView nameMessage = (TextView) customView.findViewById(R.id.textName);
+            Log.d("ISDJ", "TRUE");
+            customView = davidsinflater.inflate(R.layout.custom_row_dj, parent,false);
+            TextView textMessage = (TextView) customView.findViewById(R.id.textviewCustomDJ);
             textMessage.setText(json.get("msg").toString());
-            nameMessage.setText("From  " + json.get("name").toString() + ": ");
+            Log.d("MSG", textMessage.getText().toString());
+//            TextView nameMessage = (TextView) customView.findViewById(R.id.textNameDJ);
+//            nameMessage.setText("From  " + json.get("name").toString() + ": ");
         }
         return  customView;
     }
