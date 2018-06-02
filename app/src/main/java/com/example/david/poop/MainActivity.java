034d0d85fbdc5e432619e21649afda9ccd72672d
David <daastewa@ucsc.edu>
2018-06-02 01:27:58 -0700

    Merged RadioStreaming..lost color scheme of main activity :(

--- app/src/main/java/com/example/david/poop/MainActivity.java
+++ app/src/main/java/com/example/david/poop/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.david.poop;
 
-import android.app.Activity;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
@@ -13,21 +16,27 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
-    private Intent intent;
+    MediaPlayer mediaPlayer = new MediaPlayer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        intent = new Intent(MainActivity.this, Activity.class);
-
         setTitle("KZSC App");
 
         BottomNavigationView bottomNav = (BottomNavigationView) findViewById(R.id.bottom_navigation);
@@ -43,8 +52,56 @@ public class MainActivity extends AppCompatActivity {
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
 
+
+        final String AudioURL = "http://188.165.192.5:8242/kzschigh?type=.mp3";
+
+        //Preparing audio
+        try {
+            mediaPlayer.setDataSource(AudioURL);
+            mediaPlayer.prepareAsync();
+        } catch (IllegalArgumentException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (SecurityException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IllegalStateException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch(RuntimeException e) {
+            e.printStackTrace();
+        }
+
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_play, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()) {
+            case R.id.action_favorite:
+                if (mediaPlayer.isPlaying()) {
+                    mediaPlayer.pause();
+                    item.setIcon(R.drawable.ic_play_arrow_white_24dp);
+
+                }else{
+                    mediaPlayer.start();
+                    item.setIcon(R.drawable.ic_pause_white_24dp);
+                }
+                return true;
+
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListner =
@@ -58,13 +115,9 @@ public class MainActivity extends AppCompatActivity {
                             selectedFragment = new HomeFragment();
                             break;
                         case R.id.nav_chat:
-                            //Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-//                            startActivity(intent);
-//                            return true;
                             selectedFragment = new LoginFragment();
                             break;
                         case R.id.nav_schedule:
-                            //selectedFragment = new ScheduleFragment();
                             selectedFragment = new ScheduleFragment();
                             break;
                         case R.id.nav_about:
@@ -80,4 +133,5 @@ public class MainActivity extends AppCompatActivity {
                     return true;
                 }
             };
+
 }
